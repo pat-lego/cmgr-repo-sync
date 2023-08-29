8459f489a60ac162038832bd5a3f16741e9aa5f0
Patrique Legault <patrique.legault@gmail.com>
2023-08-29 12:25:05 -0400

    Adding sync handler

---
+++ .github/scripts/deploy-to-cmgr.sh
@@ -0,0 +1,11 @@
+# Go Home
+cd ~
+
+# Print Current Location
+echo `pwd`
+
+# Git Remote Add
+git remote add cmgr https://$USERNAME:$PASSWORD@git.cloudmanager.adobe.com/aemsupport/cmgr-sync/
+
+# Git Push to Remote cmgr
+git push -u cmgr master
