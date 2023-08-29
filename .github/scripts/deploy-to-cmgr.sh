c12d06fc11c0fba1f3c3a1c0c7e12920c42c97ed
Patrique Legault <patrique.legault@gmail.com>
2023-08-29 12:41:32 -0400

    Force push

--- .github/scripts/deploy-to-cmgr.sh
+++ .github/scripts/deploy-to-cmgr.sh
@@ -5,5 +5,8 @@ echo `pwd`
 # Git Remote Add
 git remote add cmgr https://$USERNAME:$PASSWORD@git.cloudmanager.adobe.com/aemsupport/cmgr-sync/
 
+git fetch --unshallow origin
+git fetch --unshallow cmgr
+
 # Git Push to Remote cmgr
 git push -u cmgr main --force
