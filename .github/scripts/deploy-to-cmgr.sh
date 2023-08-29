ad8ba79f16f0e4a7fe64838f7fce2a9095d407ab
Patrique Legault <patrique.legault@gmail.com>
2023-08-29 12:36:36 -0400

    Reset

--- .github/scripts/deploy-to-cmgr.sh
+++ .github/scripts/deploy-to-cmgr.sh
@@ -2,6 +2,11 @@
 # Print Current Location
 echo `pwd`
 
+# Reset the history
+
+rm -rf .git
+git init .
+
 # Git Remote Add
 git remote add cmgr https://$USERNAME:$PASSWORD@git.cloudmanager.adobe.com/aemsupport/cmgr-sync/
 
