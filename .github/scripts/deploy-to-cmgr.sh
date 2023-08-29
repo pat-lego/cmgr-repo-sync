fc6ae72efea58b197e29a8abac6da3e4f349768a
Patrique Legault <patrique.legault@gmail.com>
2023-08-29 12:37:40 -0400

    Set branch name

--- .github/scripts/deploy-to-cmgr.sh
+++ .github/scripts/deploy-to-cmgr.sh
@@ -3,12 +3,14 @@
 echo `pwd`
 
 # Reset the history
-
 rm -rf .git
 git init .
 
 # Git Remote Add
 git remote add cmgr https://$USERNAME:$PASSWORD@git.cloudmanager.adobe.com/aemsupport/cmgr-sync/
+
+# Set the branch name
+git branch -m main
 
 # Git Push to Remote cmgr
 git push -u cmgr main
